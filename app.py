c086bd5aeb12e008cf848ec18ee91f0bd9c56d21
Kesller <81837706+Kesller027@users.noreply.github.com>
2026-09-15 19:09:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
+
+API_KEY = os.getenv("API_FOOTBALL_KEY")
+
 
 @app.get("/")
 def home():
@@ -10,8 +14,26 @@ def home():
         "service": "bet-analyzer"
     }
 
+
 @app.get("/health")
 def health():
     return {
         "status": "ok"
+    }
+
+
+@app.get("/api-test")
+async def api_test():
+    url = "https://v3.football.api-sports.io/status"
+
+    headers = {
+        "x-apisports-key": API_KEY
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+
+    return {
+        "status_code": response.status_code,
+        "api_response": response.json()
     }
