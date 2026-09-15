2a844c7bd333306777a774702622387eef8690c8
Kesller <81837706+Kesller027@users.noreply.github.com>
2026-09-15 20:12:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 API_KEY = os.getenv("API_FOOTBALL_KEY")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 
 @app.get("/")
@@ -17,9 +18,7 @@ def home():
 
 @app.get("/health")
 def health():
-    return {
-        "status": "ok"
-    }
+    return {"status": "ok"}
 
 
 @app.get("/api-test")
@@ -36,4 +35,22 @@ async def api_test():
     return {
         "status_code": response.status_code,
         "api_response": response.json()
+    }
+
+
+@app.get("/telegram-test")
+async def telegram_test():
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": "-867855514",
+        "text": "🤖 Bet Analyzer: teste do Telegram funcionando!"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, data=data)
+
+    return {
+        "status_code": response.status_code,
+        "telegram_response": response.json()
     }
